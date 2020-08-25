26e62d4623e1636581174e488540926ffad5be15
sairamaj <sairamaj@gmail.com>
2020-08-25 09:26:02 -0700

    finishing the game

---
+++ javatoh/Step11/src/Application.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Application {
+
+	public static void main(String[] args) {
+		Gamer g = new Gamer();
+		try {
+			g.setup();
+		} catch (Exception e1) {
+			e1.printStackTrace();
+			return;
+		}
+
+		// for
+		boolean quit=false;
+		
+		do {
+			System.out.println("Please enter command(quit or move):");
+			
+			Scanner scanner = new Scanner(System.in);
+			String command = scanner.next();
+			
+			if(command.equals("move")) {
+				System.out.print("Please enter from which tower, you want to move:");
+				int from = scanner.nextInt();
+				System.out.print("Please enter to which tower, you want to move:");
+				int to = scanner.nextInt();
+				
+				System.out.println("we are going to move:" + from + " to:" + to);
+				try
+				{
+					boolean isGameDone = g.move(from, to);
+					if(isGameDone) {
+						System.out.println("---------------------");
+						int moveCount = g.getNumberOfMoves();
+						System.out.println("congratulations! :" + moveCount);
+						System.out.println("---------------------");
+						quit = true;
+					}
+				}
+				catch(Exception e) {
+					System.out.println(e.getMessage());
+				}
+			}else if(command.equals("quit")) {
+				quit = true;
+			}else {
+				System.out.println("wrong command(please enter move/quit");
+			}
+			
+		}while(quit==false);
+		
+		System.out.println("bye");
+	}
+
+}
---
+++ javatoh/Step11/src/Tower.java
@@ -0,0 +1,92 @@
+import java.util.*;
+
+public class Tower {
+	private String name;
+	private ArrayList<Disk> disks;
+
+	Tower(String name) {
+		this.name = name;
+		disks = new ArrayList<Disk>();
+	}
+
+	int getDiskCount() {
+		int totalDisks = this.disks.size();
+		return totalDisks;
+	}
+	
+	void addDisk(Disk d) throws Exception {
+		
+		if( this.disks.size() == 0) {
+			this.disks.add(d);	// no disk exists, so any disk allowed.
+			return;
+		}
+		
+		// input_disk_size = get the input disk(d) size
+		// topDis = get the top disk
+		// topDiskSize = get top disk size
+		// if input disk size > topDisSize
+			// if input_disk_size > topDiskSize
+				// we should throw exception
+			// else
+				// we can add
+		int input_disk_size = d.getSize();
+		
+		int totalDisks = this.disks.size();
+		int topDiskIndex = totalDisks-1;
+		Disk topDisk = this.disks.get(topDiskIndex);
+		int topDiskSize = topDisk.getSize();
+		
+		if( input_disk_size > topDiskSize) {
+			Exception notAllowed;
+			notAllowed = new Exception("Tower " + this.name + " does not allow " + input_disk_size + " to come on to " + topDiskSize);
+			throw notAllowed;
+		}
+		
+		// now allowed , so add it.
+		this.disks.add(d);
+	}
+	
+	void draw() {
+		// we drew the pole
+		drawPole();
+		
+		for(int i=this.disks.size()-1; i >=0 ; i--) {
+			Disk d = this.disks.get(i);
+			d.draw();
+		}
+
+		
+		// draw base 
+		drawBase();
+		
+		// title
+		drawTitle();
+	}
+
+	Disk removeDisk() throws Exception{
+		if( this.disks.size() == 0) {
+			Exception noDisk;
+			noDisk = new Exception("Tower: " + this.name + " does not have any disks");
+			throw noDisk;
+		}
+		
+		int totalDisks = this.disks.size();
+		int topDiskIndex = totalDisks-1;
+		Disk topDisk = this.disks.remove(topDiskIndex);
+		return topDisk;
+	}
+	
+	void drawPole() {
+		for (int i = 0; i < 4; i++) {
+			System.out.println("     |");
+		}
+	}
+
+	private void drawBase() {
+		System.out.println("________________");
+	}
+
+	private void drawTitle() {
+		System.out.println("   " + this.name);
+	}
+}
