8e55c7c4e0052ccf06a29b4169c05e2d73b68403
sairamaj <sairamaj@gmail.com>
2020-08-30 10:45:41 -0700

    adding colored disks toh

---
+++ pythontoh/Step8/gamer.py
@@ -0,0 +1,64 @@
+from tower import Tower
+from disk import Disk
+from colorama import Fore, Back, Style
+
+class Gamer:
+    # constructor called during Gamer()
+    def __init__(self):
+
+        # we create bag for towers
+        self.towers_bag = []
+
+        # create
+        towerA = Tower("Tower-A")
+
+        # create disk
+        large = Disk(12, Fore.BLUE)
+        towerA.add_disk(large)
+
+        medium = Disk(8, Fore.RED)
+        towerA.add_disk(medium)
+
+        small = Disk(4,Fore.GREEN)
+        towerA.add_disk(small)
+
+
+        # put towerA in the bag
+        self.towers_bag.append(towerA)
+
+        towerB = Tower("Tower-B")
+        # put towerB in the bag
+        self.towers_bag.append(towerB)
+
+        towerC = Tower("Tower-C")
+        # put towerC in the bag
+        self.towers_bag.append(towerC)
+
+    def draw(self):
+        # draw the towers from bag.
+        for tower in self.towers_bag:
+            tower.draw()
+
+    def move(self, from_tower, to_tower):
+        # validate the input
+        if from_tower < 1 or from_tower >3:
+            raise ValueError(f"from tower: {from_tower} should be between 1 and 3")
+        
+        if to_tower < 1 or to_tower >3:
+            raise ValueError(f"to tower: {to_tower} should be between 1 and 3")
+
+        # in program world the list indexes starts with 0
+        # For user the towers are 1,2,3 and hence we will be reducing by 1 for whatever user entered.        
+        from_tower = from_tower-1
+        to_tower = to_tower-1
+        
+        # get towers from the bag (list)
+        towerFrom = self.towers_bag[from_tower]
+        towerTo = self.towers_bag[to_tower]
+
+        # Move (remove a disk from tower, and add to tower )
+        disk = towerFrom.remove_disk()
+        towerTo.add_disk(disk)
+
+        # we are redrwing to show the new towers with disk
+        self.draw()
---
+++ pythontoh/Step8/tower.py
@@ -0,0 +1,64 @@
+from disk import Disk
+
+
+class Tower:
+
+    # Constructor. which will be called during construction
+    # which is Tower("some title")
+    def __init__(self, title):  
+        self.disk_bag = []
+        self.title = title      # automatially adding a property to object
+
+    def add_disk(self, disk):
+
+        if len(self.disk_bag) == 0:
+            self.disk_bag.append(disk)  # if no disks , we can add any disk
+            return
+        
+        # get top disk size
+        # get input disk size
+        # if input disk size > top disk size
+            # raise error
+        total_disks = len(self.disk_bag)
+        top_disk = self.disk_bag[total_disks-1]
+        top_disk_size = top_disk.get_size()
+        coming_disk_size = disk.get_size()
+
+        if coming_disk_size > top_disk_size:
+            raise ValueError(f"{self.title} disk:{coming_disk_size} cannot go on to {top_disk_size}")
+
+        self.disk_bag.append(disk)
+
+    def remove_disk(self):
+        if len(self.disk_bag) == 0:
+            raise ValueError(f'tower {self.title} does not have any disks to move.')
+
+        # get the top disk
+        total_disks = len(self.disk_bag)
+        topDisk = self.disk_bag[total_disks-1]
+        self.disk_bag.remove(topDisk)
+        return topDisk
+
+    def drawPole(self):
+        for x in range(1, 5, 1):
+            print('                       |')
+
+    def drawBase(self):
+        print('           ---------------------')
+
+    def drawTitle(self):
+        print(f"                    {self.title}")
+
+    def draw(self):
+        # pole
+        self.drawPole()
+
+        # drawing disks
+        for disk in self.disk_bag[::-1]:    # walking reverse
+            disk.draw()
+
+        # now base
+        self.drawBase()
+
+        # title now.
+        self.drawTitle()
